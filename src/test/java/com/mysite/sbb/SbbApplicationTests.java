541e211082e21df668bf20db42830ea0651db520
김지호 <jiho001108@gmail.com>
2024-01-06 17:47:57 +0900

    setSubjectTest

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,8 +1,8 @@
 package com.mysite.sbb;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.List;
+import java.util.Optional;
 
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionRepository;
@@ -19,8 +19,10 @@ class SbbApplicationTests {
 
     @Test
     void testJpa() {
-        List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
-        Question q = qList.get(0);
-        assertEquals("sbb가 무엇인가요?", q.getSubject());
+        Optional<Question> oq = this.questionRepository.findById(1); /* 질문 엔티티의 데이터를 조회 */
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+        q.setSubject("수정된 제목"); /* update */
+        this.questionRepository.save(q);
     }
 }
