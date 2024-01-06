f8d2c6242d235e857c4bdfdd6508b725e110318e
김지호 <jiho001108@gmail.com>
2024-01-06 21:27:44 +0900

    getAnswerListTest

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -3,30 +3,36 @@ package com.mysite.sbb;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.mysite.sbb.answer.Answer;
-import com.mysite.sbb.answer.AnswerRepository;
+import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionRepository;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
+/* 질문을 조회한 후 이 질문에 달린 답변 전체를 구하는 테스트 코드 */
 @SpringBootTest
 class SbbApplicationTests {
 
     @Autowired
     private QuestionRepository questionRepository;
 
-    @Autowired
-    private AnswerRepository answerRepository;
-
+    /* @Transactional: 메서드가 종료될 때까지 DB 세션이 유지 */
+    @Transactional
     @Test
     void testJpa() {
-        Optional<Answer> oa = this.answerRepository.findById(1);
-        assertTrue(oa.isPresent());
-        Answer a = oa.get();
-        assertEquals(2, a.getQuestion().getId());
+        Optional<Question> oq = this.questionRepository.findById(2);
+        assertTrue(oq.isPresent()); /* oq에서 존재하면, Question이 null값이 아니면 */
+        Question q = oq.get(); /* Question 값을 꺼냄 */
+
+        List<Answer> answerList = q.getAnswerList();
+
+        assertEquals(1, answerList.size());
+        assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
     }
 }
