752bbdb38bbe460af5dd7745d776153447799f9c
김지호 <jiho001108@gmail.com>
2024-01-08 16:17:36 +0900

    SecurityConfig: 시큐리티 설정 1. 로그인X 시에도 모든 페이지 접근 허용 2. H2 서버 예외처리

--- src/main/java/com/mysite/sbb/SecurityConfig.java
+++ src/main/java/com/mysite/sbb/SecurityConfig.java
@@ -14,16 +14,24 @@ import org.springframework.security.web.header.writers.frameoptions.XFrameOption
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableMethodSecurity(prePostEnabled = true)
-@Configuration
-@EnableWebSecurity
+@Configuration /* 스프링의 환경 설정 파일임을 의미하는 애너테이션 */
+@EnableWebSecurity /* 모든 요청 URL이 스프링 시큐리티의 제어를 받도록 만드는 애너테이션, 스프링 시큐리티를 활성화하는 역할 */
 public class SecurityConfig {
-    @Bean
+    @Bean /* 스프링 시큐리티의 세부 설정은 @Bean 통해 SecurityFilterChain 빈을 생성하여 설정 */
+    /* SecurityFilterChain클래스: 모든 요청 URL에 이 클래스가 필터로 적용되어 URL별로 특별한 설정을 할 수 있게 된다. */
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        /* 세부 설정 시작 */
         http
+                /* 인증되지 않은 모든 페이지의 요청을 허락, 로그인X -> 모든 페이지 접근 가능 */
             .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
                 .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
+
+                /* CSRF 처리 시 H2 콘솔은 예외로 처리 */
             .csrf((csrf) -> csrf
                 .ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")))
+
+                /* X-Frame-Options 헤더를 DENY 대신 SAMEORIGIN으로 설정
+                * -> 프레임에 포함된 웹 페이지가 동일한 사이트에서 제공할 때에만 사용이 허락 */
             .headers((headers) -> headers
                 .addHeaderWriter(new XFrameOptionsHeaderWriter(
                     XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
